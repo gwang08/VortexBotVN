3eeec4bdb7bc76b0cec0c7c37d2439b6688ac558
Gwang <114713193+gwang08@users.noreply.github.com>
2026-03-24 10:05:33 +0700

    change link myfx

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -9,7 +9,7 @@ export const IB_CODE = 'bmrmaster';
 
 // External links
 export const BOT_TRADING_URL = 'https://bmrcopytrade.vn/';
-export const MYFXBOOK_URL = 'https://www.myfxbook.com/members/KevinNguyen_MMF/bmr-scalper-gold/11777645';
+export const MYFXBOOK_URL = 'https://www.myfxbook.com/members/bmrmaster/bmr-scalper-gold/11980201';
 export const CHANNEL_URL = 'https://t.me/BMRCopytradevn';
 
 // Follow-up cooldown schedule (hours between each follow-up)
